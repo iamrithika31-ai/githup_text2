49e0e5d1a76e65dadae2849813e915e94122de0b
iamrithika31-ai <iamrithika31@gmail.com>
2025-12-21 20:18:41 +0530

    Add githubtext as normal folder

---
+++ githubtext/desktop_alert.py
@@ -0,0 +1,19 @@
+from plyer import notification
+from datetime import datetime
+import time
+import mysql.connector
+import platform
+
+db = mysql.connector.connect(host="localhost", user="root", password="rithi272", database="emp_db")
+cursor = db.cursor(dictionary=True)
+
+while True:
+    now = datetime.now().strftime("%H:%M")
+    cursor.execute("SELECT * FROM alerts WHERE shown=0 AND time=%s", (now,))
+    alerts = cursor.fetchall()
+    for alert in alerts:
+        if platform.system() in ["Windows","Linux","Darwin"]:
+            notification.notify(title="🔔 Alert", message=alert["message"], timeout=10)
+        cursor.execute("UPDATE alerts SET shown=1 WHERE id=%s", (alert["id"],))
+        db.commit()
+    time.sleep(30)  # check every 30 sec
